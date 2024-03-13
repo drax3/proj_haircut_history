97316edd9b3c452ddaff6652018a4aac2c8edea2
Tushar <tsompura0006@gmail.com>
2024-03-13 14:35:54 +0530

    advacne user registration minus email authentication

--- haircuts/haircuts_project/urls.py
+++ haircuts/haircuts_project/urls.py
@@ -23,9 +23,12 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     
     # User management
-    path('accounts/',include('django.contrib.auth.urls')),
+    # path('accounts/',include('django.contrib.auth.urls')),
+    path('accounts/',include('allauth.urls')),
+
 
     # Local apps
+    # path('accounts/', include('users.urls')),
     path('', include('pages.urls')),
     path('haircuts/', include('haircuts.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
